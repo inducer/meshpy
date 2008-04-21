49717930eb795e6b2edb735f91df22cb9bd79d28
Andreas Kloeckner <inform@tiker.net>
2008-04-21 02:03:06 -0400

    Upgrade to aksetup.

--- setup.py
+++ setup.py
@@ -1,27 +1,29 @@
 #!/usr/bin/env python
 
-import sys
+def get_config_schema():
+    from aksetup_helper import ConfigSchema, Option, \
+            IncludeDir, LibraryDir, Libraries, \
+            Switch, StringListOption
+
+    return ConfigSchema([
+        IncludeDir("BOOST", []),
+        LibraryDir("BOOST", []),
+        Libraries("BOOST_PYTHON", ["boost_python-gcc42-mt"]),
+
+        StringListOption("CXXFLAGS", [], 
+            help="Any extra C++ compiler options to include"),
+        ])
+
+
+
 
 def main():
-    try:
-        conf = {}
-        execfile("siteconf.py", conf)
-    except IOError:
-        print "*** Please run configure first."
-        sys.exit(1)
+    import glob
+    from aksetup_helper import hack_distutils, \
+            get_config, setup, Extension
 
-    from distutils.core import setup,Extension
-
-    def non_matching_config():
-        print "*** The version of your configuration template does not match"
-        print "*** the version of the setup script. Please re-run configure."
-        sys.exit(1)
-
-    if "MESHPY_CONF_TEMPLATE_VERSION" not in conf:
-        non_matching_config()
-
-    if conf["MESHPY_CONF_TEMPLATE_VERSION"] != 1:
-        non_matching_config()
+    hack_distutils()
+    conf = get_config()
 
     triangle_macros = [
       ( "EXTERNAL_TEST", 1 ),
@@ -34,9 +36,9 @@ def main():
       ("SELF_CHECK", 1) ,
       ]
 
-    INCLUDE_DIRS = conf["BOOST_INCLUDE_DIRS"]
-    LIBRARY_DIRS = conf["BOOST_LIBRARY_DIRS"]
-    LIBRARIES = conf["BPL_LIBRARIES"]
+    INCLUDE_DIRS = conf["BOOST_INC_DIR"]
+    LIBRARY_DIRS = conf["BOOST_LIB_DIR"]
+    LIBRARIES = conf["BOOST_PYTHON_LIBNAME"]
 
     execfile("src/python/__init__.py", conf)
     setup(name="MeshPy",
@@ -72,25 +74,4 @@ def main():
 
 
 if __name__ == '__main__':
-    # hack distutils.sysconfig to eliminate debug flags
-    # stolen from mpi4py
-    import sys
-    if not sys.platform.lower().startswith("win"):
-        from distutils import sysconfig
-
-        cvars = sysconfig.get_config_vars()
-        cflags = cvars.get('OPT')
-        if cflags:
-            cflags = cflags.split()
-            for bad_prefix in ('-g', '-O', '-Wstrict-prototypes'):
-                for i, flag in enumerate(cflags):
-                    if flag.startswith(bad_prefix):
-                        cflags.pop(i)
-                        break
-                if flag in cflags:
-                    cflags.remove(flag)
-            cflags.append("-O3")
-            cvars['OPT'] = str.join(' ', cflags)
-            cvars["CFLAGS"] = cvars["BASECFLAGS"] + " " + cvars["OPT"]
-    # and now call main
     main()
