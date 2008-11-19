2ca41294bd8c9c0429554528e3e583f5b9ca5bf4
Andreas Kloeckner <kloeckner@haamster.rice.edu>
2008-11-19 02:23:23 -0600

    Allow making boxes around PLY meshes.

---
+++ test/MESH_README.txt
@@ -0,0 +1,2 @@
+The mesh 'ka-6d.ply' is a derivative work of a mesh in FlightGear and is thus
+licensed under the GNU GPL.
--- test/mesh_ply.py
+++ test/mesh_ply.py
@@ -5,9 +5,49 @@ def main():
 
     from meshpy.tet import MeshInfo, build, Options
 
+    #points, facets, facet_holestarts, markers =
+
+    
+    import numpy
+    ply_points = numpy.array([pt[:3] for pt in data["vertex"].data])
+    ply_facets = numpy.array([fd[0] for fd in data["face"].data])
+    a, b = numpy.min(ply_points, axis=0), numpy.max(ply_points, axis=0)
+
+    a = a - 1
+    b = b + 1
+
+    #    7--------6
+    #   /|       /|
+    #  4--------5 |  z
+    #  | |      | |  ^
+    #  | 3------|-2  | y
+    #  |/       |/   |/
+    #  0--------1    +--->x
+
+    bbox_points = [
+            (a[0],a[1],a[2]),
+            (b[0],a[1],a[2]),
+            (b[0],b[1],a[2]),
+            (a[0],b[1],a[2]),
+            (a[0],a[1],b[2]),
+            (b[0],a[1],b[2]),
+            (b[0],b[1],b[2]),
+            (a[0],b[1],b[2]),
+            ]
+
+    bbox_facets = [
+            (0,1,2,3),
+            (0,1,5,4),
+            (1,2,6,5),
+            (7,6,2,3),
+            (7,3,0,4),
+            (4,5,6,7)
+            ]
+
     mi = MeshInfo()
-    mi.set_points([pt[:3] for pt in data["vertex"].data])
-    mi.set_facets([fd[0] for fd in data["face"].data])
+    mi.set_points(bbox_points+list(ply_points))
+    mi.set_facets(bbox_facets+list((ply_facets + len(bbox_points))))
+    mi.set_holes([(a+b)/2])
     mesh = build(mi)
     mesh.write_vtk("out.vtk")
 
