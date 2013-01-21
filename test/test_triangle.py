bbe6f43ae72736a25f2a31d1e6687a0d0aaf82fd
Andreas Kloeckner <inform@tiker.net>
2013-01-21 13:00:04 -0500

    Replace test_triangle.py with a slightly more streamlined example.

--- test/test_triangle.py
+++ test/test_triangle.py
@@ -1,48 +1,41 @@
+from __future__ import division
+
+import meshpy.triangle as triangle
+import numpy as np
+import numpy.linalg as la
+
+def round_trip_connect(start, end):
+  return [(i, i+1) for i in range(start, end)] + [(end, start)]
+
 def main():
-    import meshpy.triangle as triangle
-    import math
+    points = [(1,0),(1,1),(-1,1),(-1,-1),(1,-1),(1,0)]
+    facets = round_trip_connect(0, len(points)-1)
 
-    segments = 10
+    circ_start = len(points)
+    points.extend(
+            (3 * np.cos(angle), 3 * np.sin(angle))
+            for angle in np.linspace(0, 2*np.pi, 30, endpoint=False))
 
-    points = [ (1,0),(1,1),(-1,1),(-1,-1),(1,-1),(1,0) ]
+    facets.extend(round_trip_connect(circ_start, len(points)-1))
 
-    for i in range( 0, segments + 1 ):
-      angle = i * 2 * math.pi / segments
-      points.append( ( 0.5 * math.cos( angle ), 0.5 * math.sin( angle ) ) )
-
-    def round_trip_connect(start, end):
-      result = []
-      for i in range(start, end):
-        result.append((i, i+1))
-      result.append((end, start))
-      return result
-
-    def needs_refinement(vertices, area ):
-        vert_origin, vert_destination, vert_apex = vertices
-        bary_x = (vert_origin.x + vert_destination.x + vert_apex.x) / 3
-        bary_y = (vert_origin.y + vert_destination.y + vert_apex.y) / 3
-
-        dist_center = math.sqrt( bary_x**2 + bary_y**2 )
-        max_area = math.fabs( 0.002 * (dist_center-0.5) ) + 0.0001
-        return area > max_area
+    def needs_refinement(vertices, area):
+        bary = np.sum(np.array(vertices), axis=0)/3
+        max_area = 0.001 + (la.norm(bary, np.inf)-1)*0.01
+        return bool(area > max_area)
 
     info = triangle.MeshInfo()
     info.set_points(points)
     info.set_holes([(0,0)])
-    info.set_facets(round_trip_connect(0, len(points)-1))
+    info.set_facets(facets)
 
     mesh = triangle.build(info, refinement_func=needs_refinement)
 
-    import numpy as np
-    #np.set_printoptions(threshold=100000)
-    print repr(np.array(mesh.points))
-    print repr(np.array(mesh.elements))
+    mesh_points = np.array(mesh.points)
+    mesh_tris = np.array(mesh.elements)
 
-    #import pickle
-    #pickled = pickle.dumps(mesh)
-    #mesh_2 = pickle.loads(pickled)
-
-    triangle.write_gnuplot_mesh("triangles.dat", mesh)
+    import matplotlib.pyplot as pt
+    pt.triplot(mesh_points[:, 0], mesh_points[:, 1], mesh_tris)
+    pt.show()
 
 if __name__ == "__main__":
     main()
