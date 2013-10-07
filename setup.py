4177ed2deac71eed1795786b931b2aabef14dd0d
Andreas Kloeckner <inform@tiker.net>
2013-10-07 14:25:33 -0500

    Disable optimization in build, appears to cause issues/hangs on AMD

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def main():
 
     check_git_submodules()
 
-    hack_distutils()
+    hack_distutils(what_opt=1)
     conf = get_config(get_config_schema())
 
     TRI_EXTRA_OBJECTS, TRI_EXTRA_DEFINES = \
