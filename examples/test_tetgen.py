933399a5a666979ba2e6d0e2c304d6e788342e5e
Andreas Kloeckner <inform@tiker.net>
2015-03-20 18:53:33 -0500

    PEP8

--- examples/test_tetgen.py
+++ examples/test_tetgen.py
@@ -1,29 +1,27 @@
 from meshpy.tet import MeshInfo, build
 
 
-
-
 def main():
     mesh_info = MeshInfo()
 
     mesh_info.set_points([
-        (0,0,0),
-        (2,0,0),
-        (2,2,0),
-        (0,2,0),
-        (0,0,12),
-        (2,0,12),
-        (2,2,12),
-        (0,2,12),
+        (0, 0, 0),
+        (2, 0, 0),
+        (2, 2, 0),
+        (0, 2, 0),
+        (0, 0, 12),
+        (2, 0, 12),
+        (2, 2, 12),
+        (0, 2, 12),
         ])
 
     mesh_info.set_facets([
-        [0,1,2,3],
-        [4,5,6,7],
-        [0,4,5,1],
-        [1,5,6,2],
-        [2,6,7,3],
-        [3,7,4,0],
+        [0, 1, 2, 3],
+        [4, 5, 6, 7],
+        [0, 4, 5, 1],
+        [1, 5, 6, 2],
+        [2, 6, 7, 3],
+        [3, 7, 4, 0],
         ])
 
     mesh_info.save_nodes("bar")
@@ -36,8 +34,6 @@ def main():
     mesh.save_faces("barout")
 
     mesh.write_vtk("test.vtk")
-    
-
 
 
 if __name__ == "__main__":
