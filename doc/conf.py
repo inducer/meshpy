f9c69dd094e697f2afc3b1ca575a741cdfe5a962
Andreas Kloeckner <inform@tiker.net>
2015-10-17 14:21:53 -0500

    Minor doc conf changes

--- doc/conf.py
+++ doc/conf.py
@@ -32,8 +32,13 @@ extensions = [
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
-# The suffix of source filenames.
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
+# source_suffix = ['.rst', '.md']
 source_suffix = '.rst'
+
+# The encoding of source files.
+#source_encoding = 'utf-8-sig'
 
 # The master toctree document.
 master_doc = 'index'
