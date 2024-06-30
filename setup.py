6585f35adc6ccc66d02e65260cf1c7891b845405
Alexandru Fikl <alexfikl@gmail.com>
2024-06-30 15:25:57 +0300

    pyproject: move project config

--- setup.py
+++ setup.py
@@ -47,45 +47,7 @@ def main():
     init_filename = "meshpy/__init__.py"
     exec(compile(open(init_filename, "r").read(), init_filename, "exec"), conf)
 
-    setup(name="MeshPy",
-          version=conf["version"],
-          description="Triangular and Tetrahedral Mesh Generator",
-          long_description=open("README.rst", "r", encoding="utf-8").read(),
-          long_description_content_type="text/x-rst",
-          author="Andreas Kloeckner",
-          author_email="inform@tiker.net",
-          license=("MIT for the wrapper/non-commercial for "
-              "the Triangle/GNU Affero Public License for TetGen"),
-          url="https://documen.tician.de/meshpy",
-          classifiers=[
-              "Development Status :: 4 - Beta",
-              "Intended Audience :: Developers",
-              "Intended Audience :: Other Audience",
-              "Intended Audience :: Science/Research",
-              "License :: OSI Approved :: MIT License",
-              "License :: Free for non-commercial use",
-              "Natural Language :: English",
-              "Programming Language :: C++",
-              "Programming Language :: Python",
-              "Programming Language :: Python :: 3",
-              "Topic :: Multimedia :: Graphics :: 3D Modeling",
-              "Topic :: Scientific/Engineering",
-              "Topic :: Scientific/Engineering :: Mathematics",
-              "Topic :: Scientific/Engineering :: Physics",
-              "Topic :: Scientific/Engineering :: Visualization",
-              "Topic :: Software Development :: Libraries",
-              ],
-
-          packages=["meshpy"],
-          setup_requires=["pybind11"],
-          python_requires="~=3.8",
-          install_requires=[
-                  "numpy",
-                  "gmsh_interop",
-                  ],
-          tests_require=[
-                  "pytest>=2",
-                  ],
+    setup(
           ext_modules=[
               Extension(
                   "meshpy._internals",
@@ -98,7 +60,7 @@ def main():
                       "src/cpp/wrap_tetgen.cpp",
                       "src/cpp/tetgen.cpp",
                       "src/cpp/predicates.cpp",
-                      ],
+                  ],
                   include_dirs=include_dirs,
                   define_macros=triangle_macros + tetgen_macros,
                   extra_compile_args=conf["CXXFLAGS"],
